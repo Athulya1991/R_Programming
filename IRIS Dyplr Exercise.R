beb1581b0dbd743a246b582d8a95d6abc5c4f3ce
Athulya1991 <73009203+Athulya1991@users.noreply.github.com>
2021-12-02 13:23:02 -0800

    Add files via upload

---
+++ IRIS Dyplr Exercise.R
@@ -0,0 +1,78 @@
+
+# Exercises for dplyr
+
+library(dplyr)
+?iris
+names(iris)
+iris
+
+# Exercise 1
+# Select the first three columns of the iris dataset using their column names. HINT: Use select().
+
+df=iris
+df
+select(df,'Sepal.Length','Sepal.Width','Petal.Length')
+
+
+# Exercise 2
+# Select all the columns of the iris dataset except "Petal Width". HINT: Use "-".
+
+select(df,-'Petal.Width')
+
+  
+
+# Exercise 3
+# Select all columns of the iris dataset that start with the character string "P".
+
+select(df,starts_with('P'))
+
+
+# Exercise 4
+# Filter the rows of the iris dataset for Sepal.Length >= 4.6 and Petal.Width >= 0.5.
+
+filter(df,Sepal.Length>=4.6 ,Petal.Width>=0.5)
+
+# Exercise 5
+# Pipe the iris data frame to the function that will select two columns (Sepal.Width and Sepal.Length). HINT: Use pipe operator.
+
+q1=df%>%
+  select(Sepal.Length,Sepal.Width)
+q1
+
+# Exercise 6
+# Arrange rows by a particular column, such as the Sepal.Width. HINT: Use arrange().
+df
+arrange(df,Sepal.Width)
+
+# Exercise 7
+# Select three columns from iris, arrange the rows by Sepal.Length, then arrange the rows by Sepal.Width.
+
+select(df,1:3) %>% arrange(df,Sepal.Length)%>% arrange(df,Sepal.Width)
+
+
+# Exercise 8
+# Create a new column called proportion, which is the ratio of Sepal.Length to Sepal.Width. HINT: Use mutate().
+
+df%>%
+  mutate(df,proportion=(Sepal.Length/Sepal.Width))
+
+
+
+# Exercise 9
+# Compute the average number of Sepal.Length, apply the mean() function to the column Sepal.Length, and call the summary value "avg_slength". HINT: Use summarize().
+
+avg_slength=df%>%
+  summarise(n(),mean(Sepal.Length))
+avg_slength
+
+
+# Exercise 10
+# Split the iris data frame by the Sepal.Length, then ask for the same summary statistics as above. HINT: Use group_by().
+
+avg_slength1=df%>%
+  group_by(Sepal.Length)%>%
+  summarise(n(),mean(Sepal.Length))
+avg_slength1
+  
+  
+
